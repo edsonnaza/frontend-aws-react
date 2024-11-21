fb0475a6363145cd82bba01de6ff9b135aeb0464
edsonnaza <edsonnaza@gmail.com>
2024-11-20 21:10:07 -0300

    Front End title modified

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Image Resizer App - [AWS-S3-LAMBDA]</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
