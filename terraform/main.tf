594fec20d7a2d6b5e2829ca5ddd509675f11f02b
edsonnaza <edsonnaza@gmail.com>
2024-11-21 14:05:23 -0300

    add: change terraform yaml file version infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,22 +12,27 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "my_bucket_policy" {
-#   bucket = aws_s3_bucket.my_bucket.id
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false  
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid       = "AddPerm"
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:GetObject"
-#         Resource  = "arn:aws:s3:::${var.bucket_name}/*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AddPerm"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject",
+                     "s3:ListBucket"]
+        Resource  = "arn:aws:s3:::${var.bucket_name}/*"
+      }
+    ]
+  })
+}
 
 #  resource "aws_s3_bucket" "this" {
 #   bucket = "edson-react-demo-app-2024"
@@ -40,4 +45,9 @@ resource "aws_s3_bucket" "my_bucket" {
 #   rule {
 #     object_ownership = "BucketOwnerPreferred"
 #   }
+# }
+# resource "aws_s3_bucket_policy" "example" {
+#   bucket = aws_s3_bucket.example.id 
+#   policy = data.aws_iam_policy_document.example.json
+
 # }
